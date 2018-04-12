3fa1a7a29a67d6d36783a4774897f7ae910b2e43
Kevin Alvarado <kalvarado8@cps.edu>
2018-04-12 18:51:29 +0000

    Add root route

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,5 +1,6 @@
 class TodoController < ApplicationController 
     def index
+    @todos = Todo.all
     end
     def show
     @todo = Todo.find_by_id(params[:id])
@@ -26,7 +27,7 @@ class TodoController < ApplicationController
     def destroy
     t = Todo.find_by_id(params[:id])
     t.destroy
-    redirect_to "/todo/index"
+    redirect_to "/."
     end
 
 
